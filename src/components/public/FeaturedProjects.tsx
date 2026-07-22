c6883f117cf0cf65b4e5aa20b16601ebe5535292
psm <git@github.com:dodan556/psm0723.git>
2026-07-23 03:09:04 +0900

    fix: build success

--- src/components/public/FeaturedProjects.tsx
+++ src/components/public/FeaturedProjects.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 import { getProjects } from "../../services/project.service";
 import type { Project } from "../../types/project";
